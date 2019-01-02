855f795bcb0ec8a3de7535b6f73ad2d2015913e8
Tarlan Ismayilsoy <terlanismayilsoy@Terlans-MacBook.local>
2019-01-02 23:51:07 +0400

    Player model added. Some button logic was applied

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,14 +10,40 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        player = Player()
+    }
+    
+    @IBAction func onMensTapped(_ sender: Any)
+    {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any)
+    {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any)
+    {
+        selectLeague(leagueType: "coed")
     }
     
     @IBAction func onNextTapped(_ sender: Any)
     {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+    func selectLeague(leagueType: String)
+    {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
     }
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Tarlan Ismayilsoy on 1/2/19.
+//  Copyright © 2019 Tarlan Ismayilsoy. All rights reserved.
+//
+
+import Foundation
+
+struct Player
+{
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
